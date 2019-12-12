bb46104ad2e8dbaffbe49cd50c27e9a601b61656
Sergei2000 <dsdem@mail.ru>
2019-12-12 18:30:03 +0300

    try10

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,8 +6,16 @@ TEST(SharedPtr, value )
 {
     int*a = new int;
     a[0] = 10;
+    int*b = new int;
+    b[0] = 9;
     SharedPtr<int> test(a);
+    EXPECT_EQ(static_cast<int>(test._adress[0]), 10);
+    SharedPtr<int> test2(test);
+    EXPECT_EQ(static_cast<int>(test2._adress[0]), 10);
+    test.reset(b);
     EXPECT_EQ(static_cast<int>(test._adress[0]), 9);
+    test.swap(test2);
+    EXPECT_EQ(static_cast<int>(test._adress[0]), 10);
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
