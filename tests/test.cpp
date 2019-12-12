ad0a4e8708431dd611885fb57ef125dd6daaa4d4
Sergei2000 <dsdem@mail.ru>
2019-12-12 18:14:47 +0300

    try6

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,16 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include "header.hpp"
+TEST(SharedPtr,value ) 
+{
+	
+    int*a =new int;
+    a[0]=10;
+    SharedPtr<int> test(a);
+    EXPECT_EQ(int(test._adress[0]),10);
+}
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
